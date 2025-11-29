281f8a76991d287cd00bdea3b932ecf37e2bfb0d
DangChuVM <dangchinhcbg@gmail.com>
2025-11-29 17:41:02 +0700

    oke

--- app/src/main/java/com/dangchuvmnewb/MainActivity.java
+++ app/src/main/java/com/dangchuvmnewb/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dangchuvmnewb;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.dangchuvmnewb.databinding.ActivityMainBinding;
@@ -14,19 +16,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.textView.setText("Chào, " + getString(R.string.app_name) + " Activity!");
-        binding.textView.setOnClickListener(v -> {
-            Toast.makeText(this, getString(R.string.app_name),Toast.LENGTH_SHORT).show(); // Lấy app name khi click vào textView
-        });
+        
+        WebView webView = binding.webview;
+        WebSettings ws = webView.getSettings();
+        
+        ws.setMediaPlaybackRequiresUserGesture(false);
+        ws.setBuiltInZoomControls(true);
+        ws.setCacheMode(WebSettings.LOAD_DEFAULT);
+        ws.setLoadWithOverviewMode(true);
+        ws.setLoadsImagesAutomatically(true);
+        ws.setJavaScriptCanOpenWindowsAutomatically(true);
+        ws.setJavaScriptEnabled(true);
+        ws.setDatabaseEnabled(true);
+        ws.setDefaultTextEncodingName("utf-8");
+        ws.setDisplayZoomControls(false);
+        ws.setSupportMultipleWindows(true);
+        ws.setSafeBrowsingEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setSupportZoom(true);
+        ws.setUseWideViewPort(true);
+        
+        binding.webview.loadUrl("https://google.com");
     }
 
     @Override
     public void onBackPressed() {
+        if(binding.webview.canGoBack()) {
+        	binding.webview.goBack();
+        } else {
         if(extime + 2000 > System.currentTimeMillis()) { //Mặc định sẽ là 2 giây
         	finish();
         } else {
         	Toast.makeText(this, getString(R.string.exitmess),Toast.LENGTH_SHORT).show();
         }
         extime = System.currentTimeMillis();
+        }
     }
 }
