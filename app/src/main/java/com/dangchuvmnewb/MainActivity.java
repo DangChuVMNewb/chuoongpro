cfd4b6559d5a126b5370608d116afb27c1715f07
DangChuVMNewb <dangchinhcbg@gmail.com>
2025-11-29 10:48:01 +0700

    fist commit

---
+++ app/src/main/java/com/dangchuvmnewb/MainActivity.java
@@ -0,0 +1,32 @@
+package com.dangchuvmnewb;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.dangchuvmnewb.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private long extime;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.textView.setText("Chào, " + getString(R.string.app_name) + " Activity!");
+        binding.textView.setOnClickListener(v -> {
+            Toast.makeText(this, getString(R.string.app_name),Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(extime + 2000 > System.currentTimeMillis()) {
+        	finish();
+        } else {
+        	Toast.makeText(this, "Nhấp lần nữa để thoát.",Toast.LENGTH_SHORT).show();
+        }
+        extime = System.currentTimeMillis();
+    }
+}
