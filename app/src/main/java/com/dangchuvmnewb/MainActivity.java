2e8bac1c0ce82d8b24a4531e83e1a8e4c8a4d697
DangChuVMNewb <dangchinhcbg@gmail.com>
2025-11-29 16:38:18 +0700

    .

--- app/src/main/java/com/dangchuvmnewb/MainActivity.java
+++ app/src/main/java/com/dangchuvmnewb/MainActivity.java
@@ -8,7 +8,7 @@ import com.dangchuvmnewb.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private long extime;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         binding.textView.setText("Chào, " + getString(R.string.app_name) + " Activity!");
         binding.textView.setOnClickListener(v -> {
-            Toast.makeText(this, getString(R.string.app_name),Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.app_name),Toast.LENGTH_SHORT).show(); // Lấy app name khi click vào textView
         });
     }
 
     @Override
     public void onBackPressed() {
-        if(extime + 2000 > System.currentTimeMillis()) {
+        if(extime + 2000 > System.currentTimeMillis()) { //Mặc định sẽ là 2 giây
         	finish();
         } else {
         	Toast.makeText(this, "Nhấp lần nữa để thoát.",Toast.LENGTH_SHORT).show();
